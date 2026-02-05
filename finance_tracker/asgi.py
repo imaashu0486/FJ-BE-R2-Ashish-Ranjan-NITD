0cf1370dcd210df21a6001ed21ba905dc35488e8
imaashu0486 <ashishranjan0486@gmail.com>
2026-02-06 01:37:59 +0530

    Fix Django core configuration and project files

---
+++ finance_tracker/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for finance_tracker project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "finance_tracker.settings"
+)
+
+application = get_asgi_application()
---
+++ finance_tracker/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for finance_tracker project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "finance_tracker.settings"
+)
+
+application = get_wsgi_application()
